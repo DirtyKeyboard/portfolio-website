37ab3d9b0cf00dc56687ccab86af1146030dc8d1
DirtyKeyboard <andrewxx000222@icloud.com>
2023-03-30 11:15:39 -0500

    finished wip page

--- src/WIP.jsx
+++ src/WIP.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
-import {Header, Button, Icon} from 'semantic-ui-react'
+import React, {useState} from 'react'
+import {Header, Button, Popup, Modal} from 'semantic-ui-react'
 
 const WIP = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div class="absCtr">
       <u style={{color: 'white'}}><Header style={{fontSize: '90px'}} as='h1' inverted>Not so fast!</Header></u>
       <Header as='h1' inverted>This site is still a <span style={{color: '#54ffff'}}>work in progress!</span></Header>
       <Header as='h2' inverted>Enjoy some links in the meantime:</Header>
-      <Button size='huge' href='https://github.com/DirtyKeyboard' circular inverted icon='github'/>
-      <Button size='huge' circular inverted icon='linkedin'/>
-      <Button size='huge' circular inverted icon='medium'/>
-      <Button size='huge' circular inverted icon='user'/>
+      <Popup content='Github' trigger={<Button size='huge' href='https://github.com/DirtyKeyboard' circular inverted icon='github'/>} />
+      <Popup content='LinkedIn' trigger={<Button size='huge' href='https://www.linkedin.com/in/andrewhawileh/' circular inverted icon='linkedin'/>} />
+      <Popup content='Blogs' trigger={<Button size='huge' href='https://medium.com/@andrewh829' circular inverted icon='medium'/>} />
+      <Popup content='Contact Me' trigger={<Button size='huge' circular inverted icon='user' onClick={() => setOpen(true)}/>} />
+      <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+    >
+      <Modal.Header>Contact Me</Modal.Header>
+      <Modal.Content>
+        <Modal.Description>
+          <h4>I look forward to hearing from you!</h4>
+          <p>E-mail: <a href="mailto:andrewh829@gmail.com">andrewh829@gmail.com</a></p>
+          <p>Phone Number: +1-(469)-744-1118</p>
+          <p><em>Best way to reach me</em>: Call/Text</p>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button
+          content="Ok"
+          labelPosition='right'
+          icon='checkmark'
+          onClick={() => setOpen(false)}
+          positive
+        />
+      </Modal.Actions>
+    </Modal>
     </div>
   )
 }
 
-export default WIP
+export default WIP 
