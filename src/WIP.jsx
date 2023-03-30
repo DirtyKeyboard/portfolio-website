859def5962dacab4f24c477906b271e8f84392af
DirtyKeyboard <andrewxx000222@icloud.com>
2023-03-30 12:56:19 -0500

    support for mobile

--- src/WIP.jsx
+++ src/WIP.jsx
@@ -6,9 +6,9 @@ const WIP = () => {
   return (
     <div class='absCtr'>
       <div class='child'>
-      <u style={{color: 'white'}}><Header style={{fontSize: '90px'}} as='h1' inverted>Not so fast!</Header></u>
-      <Header as='h1' inverted>This site is still a <span style={{color: '#54ffff'}}>work in progress!</span></Header>
-      <Header as='h2' inverted>Enjoy some links in the meantime:</Header>
+      <u style={{color: 'white'}}><Header style={{fontSize: '4.5vw'}} as='h1' inverted>Not so fast!</Header></u>
+      <Header as='h1' inverted style={{fontSize: '1.5vw'}}>This site is still a <span style={{color: '#54ffff'}}>work in progress!</span></Header>
+      <Header as='h2' inverted style={{fontSize: '1.4vw'}}>Enjoy some links in the meantime:</Header>
       <Popup content='Github' trigger={<Button size='huge' href='https://github.com/DirtyKeyboard' circular inverted icon='github'/>} />
       <Popup content='LinkedIn' trigger={<Button size='huge' href='https://www.linkedin.com/in/andrewhawileh/' circular inverted icon='linkedin'/>} />
       <Popup content='Blogs' trigger={<Button size='huge' href='https://medium.com/@andrewh829' circular inverted icon='medium'/>} />
