48c7f53730ec8d4436d0c069a9f75924669fa4b6
DirtyKeyboard <andrewxx000222@icloud.com>
2023-06-01 17:53:21 -0500

    added button spacing

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -11,7 +11,7 @@ const Landing = () => {
           <Header className="invisible" as='h1' style={{fontSize: '5vh', alignSelf: 'flex-end'}} inverted >Hello, I am <span style={{color: '#54ffff'}}>Andrew Hawileh</span></Header>
       </Grid.Row>
       <Grid.Row style={{height: '50%'}}>
-        <Header className="invisible two" as='h1' style={{fontSize: '3.5vh'}}  inverted>I am a Full Stack Software Engineer <br/><Button className="invisible three" icon='code' onAnimationEnd={() => canClick = true} style={{fontSize: '2vh'}} onClick={() => {
+        <Header className="invisible two" as='h1' style={{fontSize: '3.5vh'}}  inverted>I am a Full Stack Software Engineer <br/><Button className="invisible three" icon='code' onAnimationEnd={() => canClick = true} style={{fontSize: '2vh', marginTop: '25px'}} onClick={() => {
           if (canClick) nav('/about')}} inverted color='teal' content="View My Work"/></Header>
       </Grid.Row>
     </Grid>
